22f0ffb8aa2261e7c7bdff273f2020192a0f9356
PranshulGG <134786144+PranshulGG@users.noreply.github.com>
2023-07-06 11:27:25 +0530

    Update start.js

--- start.js
+++ start.js
@@ -1,11 +1,11 @@
 // Define the updated message
-var updatedMessage = "<p>1. New Update is here!.</p>\n<p>2. Experience. new things.</p>\n<p>3. Startup screen changed.</p>\n<p>4. Now Supports lollipop 5.0.</p>\n<p>5. New Update Notifier!</p>";
+var updatedMessage = "<p>1. Sound added to buttons!.</p>\n<p>2. Experience. new things.</p>\n<p>3. Startup screen changed.</p>\n<p>4. Now Supports lollipop v5.0.</p>\n<p>5. New Update Notifier!</p>";
 
 
 
 // Check if the user has dismissed the update or if the message has changed
 if (!localStorage.getItem('dismissed') || localStorage.getItem('message') !== updatedMessage) {
-  // Show the update container if it hasn't been dismissed or if the message has changed 
+  // Show the update container if it hasn't been dismissed or if the message has changed
   var updateContainer = document.getElementById('update-container');
   updateContainer.style.display = 'block';
 
@@ -25,4 +25,13 @@ if (!localStorage.getItem('dismissed') || localStorage.getItem('message') !== up
   // Hide the update container if it has been dismissed and the message hasn't changed
   var updateContainer = document.getElementById('update-container');
   updateContainer.style.display = 'none';
+}
+
+function playSound(event) {
+  var audio = document.getElementById("buttonSound");
+  audio.play();
+  setTimeout(function() {
+    window.location.href = event.target.href;
+  }, 300); // Adjust the delay (in milliseconds) before navigating to the new page
+  event.preventDefault();
 }
